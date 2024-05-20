d6896a254f824bd20e75ffc64458f56d97fcc751
Denzel <lawrencedenzel60@gmail.com>
2024-05-20 23:38:30 +0300

    Function called nextNumber and returns the next number

--- 0x00-Introduciton_to_JavaScript/solution.js
+++ 0x00-Introduciton_to_JavaScript/solution.js
@@ -16,3 +16,16 @@ function sum1(num1, num2){
 }
 console.log(sum1());
 
+// Return the next number from the number passed
+// Write a function called nextNumber that takes in a number as an argument, increments it by 1 to find the next number, and then returns the next number.
+// Examples:
+
+// nextNumber(1) returns 2
+// nextNumber(0) returns 1
+// nextNumber(-2) returns -1
+
+function nextNumber(number){
+    return number + 1;
+}
+
+console.log(nextNumber(1));
